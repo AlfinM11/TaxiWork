aad58f001c7c1aa21fcc5d1808e0955b48f6f609
AlfinMH <alfinmhasan07@students.amikom.ac.id>
2021-03-17 15:05:28 +0700

    Add project files.

---
+++ Taxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taksi = new Taxi();
+
+            taksi.DriverName = "Agung";
+            taksi.OnDuty = true;
+            taksi.NumPassenger = 15;
+
+            taksi.TaxiInfo();
+            taksi.PickupPassenger();
+            taksi.DropoffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi/Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        public void TaxiInfo()
+        {
+            Console.WriteLine($"Driver Name {DriverName}");
+            if (OnDuty) Console.WriteLine("On Duty : Yes");
+            else Console.WriteLine("On Duty : No");
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            Console.WriteLine();
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine($"{DriverName} Sedang menjemput penumpang");
+        }
+        public void DropoffPassenger()
+        {
+            Console.WriteLine("{0} Selesai mengantar penumpang", DriverName);
+        }
+    }
+}
